6f0ffd8818b2693fbb116e1dcaa3b263b63ffcd4
sunny882 <sany.toor@gmail.com>
2025-01-25 18:07:39 +0000

    remove deprecated message

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
